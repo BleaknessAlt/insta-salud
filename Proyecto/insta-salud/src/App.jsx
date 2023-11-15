607c17c18a04bf08f415cf3a2aa9b74296a556ee
Álvaro <128129748+blokclass@users.noreply.github.com>
2023-11-15 14:57:37 +0100

    Añadido registro a la página web

--- Proyecto/insta-salud/src/App.jsx
+++ Proyecto/insta-salud/src/App.jsx
@@ -2,18 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import InstaSaludNavbar from './Components/Navbar'
-
+import InstaSaludNavbar from "./Components/Navbar";
+import LoginModal from "./Components/LoginModal";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <InstaSaludNavbar></InstaSaludNavbar>
-
-    </>
-  );
+    return (
+        <>
+            <InstaSaludNavbar></InstaSaludNavbar>
+        </>
+    );
 }
 
 export default App;
--- Proyecto/insta-salud/src/Components/Navbar.jsx
+++ Proyecto/insta-salud/src/Components/Navbar.jsx
@@ -1,31 +1,44 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
+import { Button } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import ToggleTheme from "./ToggleTheme";
+import LoginModal from "./LoginModal";
+import { useState } from "react";
 
 function InstaSaludNavbar() {
+    const [visible, setVisibility] = useState(false);
+
+    const handleVisibility = () => setVisibility(false);
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>2
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2"></NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-                <ToggleTheme></ToggleTheme>
-            </Container>
-        </Navbar>
+        <>
+            <LoginModal show={visible} ></LoginModal>
+            <Button onClick={handleVisibility}></Button>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>2
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Inicio</Nav.Link>
+                            <Nav.Link href="#home">Especialidades</Nav.Link>
+                            <Nav.Link href="#home">Especialistas</Nav.Link>
+                            <Nav.Link href="#link">Gestión de Citas</Nav.Link>
+                            <Nav.Link onClick={handleVisibility}>Iniciar Sesión</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2"></NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    <ToggleTheme></ToggleTheme>
+                </Container>
+            </Navbar>
+        </>
     );
 }
 
